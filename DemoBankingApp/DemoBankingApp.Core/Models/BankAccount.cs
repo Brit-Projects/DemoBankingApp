7378d15118a750c8dda8c51fb4a30f99ac5ba17d
Jerome Hamilton <jehamilton567@hotmail.com>
2023-09-19 20:28:20 +0100

    Create a new account

--- DemoBankingApp/DemoBankingApp.Core/Models/BankAccount.cs
+++ DemoBankingApp/DemoBankingApp.Core/Models/BankAccount.cs
@@ -3,9 +3,11 @@ namespace DemoBankingApp.Core.Models
 {
     public class BankAccount
     {
+        public string? AccountName { get; set; }
         public int AccountId { get; set; }
         public int AccountNumber { get; set; }
-        public string SortCode { get; set; }
-        public int SecruityCode { get; set; }      
+        public int SortCode { get; set; }
+        public int SecruityCode { get; set; }
+        public decimal Balance { get; set; }
     }
 }
---
+++ DemoBankingApp/DemoBankingApp.Core/Services/IManageAccountService.cs
@@ -0,0 +1,9 @@
+﻿using DemoBankingApp.Core.Models;
+
+namespace DemoBankingApp.Core.Services
+{
+    public interface IManageAccountService
+    {
+        IDictionary<string, BankAccount>? CreateNewAccount(int id, string name, decimal initialBalance);
+    }
+}
