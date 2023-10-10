8ca96f1084e65caaf68425c84846441f45405e3c
Francesca Siema <francesca.siema@britinsurance.com>
2023-10-10 11:17:38 +0100

    REfectored together Clarice's and latest from Fran + Jerome.

--- DemoBankingApp/DemoBankingApp.Core/Services/IManageAccountService.cs
+++
@@ -1,9 +0,0 @@
-﻿using DemoBankingApp.Core.Models;
-
-namespace DemoBankingApp.Core.Services
-{
-    public interface IManageAccountService
-    {
-        IDictionary<string, BankAccount>? CreateNewAccount(int id, string name, decimal initialBalance);
-    }
-}
