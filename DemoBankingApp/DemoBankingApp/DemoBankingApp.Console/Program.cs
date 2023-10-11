897df7ce0c5ad9260b590e7a2d4b22f17b95c01b
Francesca Siema <francesca.siema@britinsurance.com>
2023-10-11 15:52:01 +0100

    Worked with Clarice to add Dashboard

---
+++ DemoBankingApp/DemoBankingApp/DemoBankingApp.Console/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DemoBankingApp.Core.Repositories;
+using DemoBankingApp.Core.Services;
+
+namespace DemoBankingApp.Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Authentication authentication = new Authentication();
+            authentication.WelcomeUser();
+
+
+            TransactionsService accountTransactionsService = new TransactionsService();
+            accountTransactionsService.DepositMoney(50, 1);
+            accountTransactionsService.GetBalance(1);
+
+ 
+
+
+        }
+    }
+}
---
+++ DemoBankingApp/DemoBankingApp/DemoBankingApp.Core/Services/DashboardService.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoBankingApp.Core;
+
+namespace DemoBankingApp.Core.Services
+{
+    internal class DashboardService
+    {
+        private TransactionsService transactionsService;
+
+        public DashboardService(TransactionsService transactionsService)
+        {
+            this.transactionsService = transactionsService;
+        }
+
+        public void DisplayDashboard()
+        {
+            Console.WriteLine("Welcome to the Dashboard");
+            Console.WriteLine("1. View Transactions");
+            Console.WriteLine("2. Make a Transaction");
+            Console.WriteLine("3. Exit");
+
+            int choice = GetUserChoice();
+
+            switch (choice)
+            {
+                case 1:
+                    ViewAccounts();
+                    break;
+
+                case 2:
+                    MakeTransaction();
+                    break;
+
+                case 3:
+                    Console.WriteLine("Goodbye!");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice. Please select a valid option.");
+                    break;
+                            
+            }
+
+
+        }
+
+        private int GetUserChoice()
+        {
+            Console.Write("Enter your choice: ");
+            if (int.TryParse(Console.ReadLine(), out int choice))
+            {
+                return choice;  
+            }
+
+            return -1;
+        }
+
+        private void ViewAccounts()
+        {
+            //var transactions = transactionService.GetTransactions();
+
+            //Console.WriteLine("Transaction:");
+            //foreach (var transaction in transactions)
+            //{
+            //    Console.WriteLine($"Transactoin ID: {transaction.TransactionId}");
+            //    Console.WriteLine($"Description: {transaction.Description}");
+            //    Console.WriteLine($"Amount: {transaction.Amount}");
+            //    Console.WriteLine();
+            
+            Console.WriteLine("Feature under construction");
+        }
+
+
+        private int GetUserChoiceTransactions()
+        {
+            Console.Write("Enter your choice: ");
+            if (int.TryParse(Console.ReadLine(), out int choiceTransactions))
+            {
+                return choiceTransactions;
+            }
+
+            return -1;
+        }
+
+        private void MakeTransaction()
+        {
+
+            Console.WriteLine("Select type of transaction");
+            Console.WriteLine("1. Deposit Funds");
+            Console.WriteLine("2. Withdraw Funds");
+            Console.WriteLine("3. Show Balance");
+            Console.WriteLine("4. Exit)");
+
+            int choiceTransactions = GetUserChoiceTransactions();
+
+            switch (choiceTransactions)
+            {
+                case 1:
+                    transactionsService.DepositMoney();
+                    break;
+
+                case 2:
+                    transactionsService.WithdrawMoney();
+                    break;
+
+                case 3:
+                    transactionsService.GetBalance();
+                    break;
+
+                case 4:
+                    Console.WriteLine("Goodbye!");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice. Please select a valid option.");
+                    break;
+            }
+    }
+}
+
